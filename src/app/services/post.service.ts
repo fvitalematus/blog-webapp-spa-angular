b7639fa684263422841b68383ee431d71fc20949
Franco <franco.vitalematus@gmail.com>
2020-07-03 00:41:52 -0400

    Listado de entradas

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -27,4 +27,10 @@ export class PostService {
 
         return this._http.post(this.url + 'post', params, { headers: headers });
     }
+
+    getPosts(): Observable<any> {
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.get(this.url + 'posts', { headers: headers });
+    }
 }
